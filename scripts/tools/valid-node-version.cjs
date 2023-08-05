6ecfbb10bb0e10b62402dfac29ec0112aabb871c
Fernando Sánchez <fernando.sanchez5@vodafone.com>
2023-08-05 22:53:48 +0200

    feat: add script validation for node version

---
+++ scripts/tools/valid-node-version.cjs
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const fs = require('fs');
+
+const nvmVersion = fs.readFileSync('.nvmrc').toString().trim();
+const desired = `v${nvmVersion}`;
+const running = process.version;
+
+if (!running.startsWith(desired)) {
+  console.error(
+    `You are running Node ${running} but version ${desired} is expected. ` +
+      `Use nvm or another version manager to install ${desired}, and then activate it.`
+  );
+  process.exit(1);
+}
