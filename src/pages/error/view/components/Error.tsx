d68fc5fb372473ef3f6c7886fc2d1b5a8fdff347
Fernando Sánchez <fernando.sanchez5@vodafone.com>
2023-08-06 20:05:28 +0200

    feat: change meta data title in index.html

--- src/pages/error/view/components/Error.tsx
+++ src/pages/error/view/components/Error.tsx
@@ -1,14 +1,11 @@
 import type { FC } from 'react';
 import { Content } from '../../../../common/view/components/content';
-import { Layout } from '../../../../common/view/components/layout';
 
 const Error: FC = () => {
   return (
-    <Layout>
-      <Content>
-        <div>ErrorElementRoot</div>
-      </Content>
-    </Layout>
+    <Content>
+      <div>ErrorElementRoot</div>
+    </Content>
   );
 };
 
