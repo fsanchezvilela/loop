7444d7d5b4a999c257cb56a8b4b7e8b1aaee796d
Fernando Sánchez <fernando.sanchez5@vodafone.com>
2023-08-12 18:26:06 +0200

    feat: add react-query to main app

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { homeRoutes } from './pages/home/router';
 import { Layout } from './common/view/components/layout';
 import './common/view/styles/global.css';
-
+// Create Router Client
 const router = createBrowserRouter([...homeRoutes]);
+// Create a Query client
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Layout>
-      <RouterProvider router={router} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <RouterProvider router={router} />
+      </Layout>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
