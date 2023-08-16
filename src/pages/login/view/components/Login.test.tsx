38a02492d0a0fb7f5a20bf3c0e95ad46cb101c4f
Fernando Sánchez <fernando.sanchez5@vodafone.com>
2023-08-16 06:43:30 +0200

    test: add the login no access_token render case

--- src/pages/login/view/components/Login.test.tsx
+++ src/pages/login/view/components/Login.test.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable import/named */
-import Home from './Login';
+import Login from './Login';
 import { screen } from '@testing-library/react';
 import { renderWithRouter } from '../../../../test-utils';
 
 describe('<Home>', () => {
   test('rendering a component that uses useLocation', () => {
     const route = '/';
-    renderWithRouter(<Home />, { route });
-    expect(screen.getByText('Children Nav to 1')).toBeTruthy();
+    renderWithRouter(<Login />, { route });
+    expect(
+      screen.getByText('Please Wait, creating access_tokens'),
+    ).toBeTruthy();
   });
 });
