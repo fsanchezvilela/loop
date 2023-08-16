efb357369ea1881707631f00dfd68d7b0b01377c
Fernando Sánchez <fernando.sanchez5@vodafone.com>
2023-08-16 02:22:10 +0200

    feat: use env client id and secret

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly SPOTIFY_CLIENT_ID: string;
+  readonly SPOTIFY_CLIENT_SECRET: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
