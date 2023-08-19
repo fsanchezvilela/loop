4a198e5884103736780cc7cf96aca8c8f15b4d8b
Fernando Sánchez <fernando.sanchez5@vodafone.com>
2023-08-19 21:30:07 +0200

    fix: wip debug connect prod body post request

--- src/auth/data/repositories/AuthRepository.ts
+++ src/auth/data/repositories/AuthRepository.ts
@@ -48,6 +48,7 @@ class AuthRepository implements IAuthRepository {
       client_id: this.client_id,
       code_verifier: codeVerifier,
     });
+    console.log({ body });
     this.authHttpClient
       .post<SpotifyAuthResponse>('https://accounts.spotify.com/api/token', body)
       .then((response) => {
